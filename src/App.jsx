72bb5d3433cd4455f28cd5ee340e74f5f05c6efb
revagraphics <media.revagraphics@gmail.com>
2026-07-09 12:08:46 +0530

    package.json

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import React from "react";
+import { Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import About from "./pages/About";
 import Service from "./pages/Service";
 import Doctors from "./pages/Doctors";
@@ -9,22 +9,26 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import GoToTop from "./components/GoToTop";
 import Social from "./components/Social";
+import NotFound from "./pages/NotFound";
+
+
 function App() {
-
-
   return (
     <div className="">
       <GoToTop />
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/service" element={<Service />} />
-          <Route path="/doctors" element={<Doctors />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-        <Footer />
-        <Social />
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/service" element={<Service />} />
+        <Route path="/doctors" element={<Doctors />} />
+        <Route path="/contact" element={<Contact />} />
+
+        {/* 404 Route - Catch all unknown paths */}
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+      <Footer />
+      <Social />
     </div>
   );
 }
